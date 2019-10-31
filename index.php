eddbca47d495c80f4e40e677486c5b188af5b60f
VideoWhisper.com <videowhispers@gmail.com>
2019-10-31 15:32:33 +0200

    Update

--- index.php
+++ index.php
@@ -60,6 +60,8 @@ $bodyCode .= '<div class="ui segment"><h4 class="ui header">Room URL</h4>Partici
 </head>
 <body>
 <?php echo $bodyCode ?>
-<div class="ui segment"><h4 class="ui header">HTML5 Videochat - Plain PHP</h4>This is a simple embedding preview edition, with simple scripts to embed app and showcase few features. Live demo for this edition is available at <a href="https://videowhisper.com/demos/html5-videochat/">HTML5 Videochat - Plain PHP - Live Demo</a>. For implementation of advanced capabilities, see <a href="https://paidvideochat.com/html5-videochat/">Turnkey HTML5 Videochat Site</a> edition, available as WordPress plugin with full php source.
+<div class="ui segment"><h4 class="ui header">HTML5 Videochat - Plain PHP</h4>This is a simple embedding preview edition, with simple scripts to embed app and showcase few features. This setup implements the room lobby with simple live video streaming (from performer to participants) and text chat.
+<br>Live demo for this edition is available at <a href="https://videowhisper.com/demos/html5-videochat/">HTML5 Videochat - Plain PHP - Live Demo</a>: accessing directly shows broadcasting interface for performer and link to invite other participants.
+<br>For implementation of advanced capabilities (including 2 way videocalls / shows, video conferencing, collaboration, tips, pay per minute, advanced tabbed interface), see <a href="https://paidvideochat.com/html5-videochat/">Turnkey HTML5 Videochat Site</a> edition, available as WordPress plugin with full php source.
 </div>
 </body>
